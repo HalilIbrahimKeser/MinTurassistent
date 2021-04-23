ca6001b2f92428002b5c04a00fe862027564dc66
Are Abrahamsen <aab057@uit.no>
2021-04-23 13:26:20 +0200

    La til integers.xml og <meta-data i  manifest

--- app/src/main/java/com/aphex/minturassistent/MainActivity.java
+++ app/src/main/java/com/aphex/minturassistent/MainActivity.java
@@ -2,7 +2,9 @@ package com.aphex.minturassistent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+  //      Configuration.getInstance().load(getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
     }
 }
