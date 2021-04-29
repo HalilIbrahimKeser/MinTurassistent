309faa989b8e7f27c39061905d1a9ad9dfe45c0c
Halil Ibrahim <hke006@uit.no>
2021-04-29 20:19:35 +0200

    Det er en feil på loginn om man ikke skriver inn bruker og passord

--- app/src/main/java/com/aphex/minturassistent/LoginFragment.java
+++ app/src/main/java/com/aphex/minturassistent/LoginFragment.java
@@ -23,7 +23,7 @@ public class LoginFragment extends Fragment {
     private static final int RC_SIGN_IN = 123;
     private static final String TAG = "TAG_EMAIL_LOGIN";
 
-    private FragmentLoginBinding fragmentLoginBinding;
+    private FragmentLoginBinding binding;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -40,68 +40,68 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        fragmentLoginBinding = FragmentLoginBinding.inflate(inflater, container, false);
-        return fragmentLoginBinding.getRoot();
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onStart() {
         super.onStart();
 
-        fragmentLoginBinding.buttonLogin.setOnClickListener(new View.OnClickListener() {
+        binding.buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mAuth.signInWithEmailAndPassword(fragmentLoginBinding.editUser.getText().toString(),
-                        fragmentLoginBinding.editPassword.getText().toString())
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Navigation.findNavController(getView()).navigate(R.id.mainFragment);
-                        } else {
-                            Toast.makeText(getContext(), "Ikke registrert bruker", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-            }
-        });
-
-        fragmentLoginBinding.buttonRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                if(!fragmentLoginBinding.editUser.getText().toString().isEmpty()) {
-                    mAuth.createUserWithEmailAndPassword(fragmentLoginBinding.editUser.getText().toString(),
-                            fragmentLoginBinding.editPassword.getText().toString())
+                if(binding.editUser.getText().toString().isEmpty()) {
+                    mAuth.signInWithEmailAndPassword(binding.editUser.getText().toString(),
+                            binding.editPassword.getText().toString())
                             .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                                 @Override
                                 public void onComplete(@NonNull Task<AuthResult> task) {
                                     if (task.isSuccessful()) {
-                                        Toast.makeText(getContext(),"Du er registrert!",Toast.LENGTH_SHORT).show();
                                         Navigation.findNavController(getView()).navigate(R.id.mainFragment);
                                     } else {
-                                        Toast.makeText(getContext(), "Kunne ikke registrere bruker.", Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(getContext(), "Ikke registrert bruker", Toast.LENGTH_SHORT).show();
                                     }
                                 }
                             });
-//                }else {
-//                    Toast.makeText(getContext(), "Du må skrive inn brukernavn og passord.", Toast.LENGTH_SHORT).show();
-//                }
+                }else {
+                    Toast.makeText(getContext(), "Du må skrive brukernavn og passord", Toast.LENGTH_SHORT).show();
+                }
 
             }
         });
 
-        fragmentLoginBinding.buttonDelete.setOnClickListener(new View.OnClickListener() {
+        binding.buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mAuth.sendPasswordResetEmail(fragmentLoginBinding.editUser.getText().toString())
+                mAuth.createUserWithEmailAndPassword(binding.editUser.getText().toString(),
+                        binding.editPassword.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getContext(), "Du er registrert!", Toast.LENGTH_SHORT).show();
+                                    Navigation.findNavController(getView()).navigate(R.id.mainFragment);
+                                } else {
+                                    Toast.makeText(getContext(), "Kunne ikke registrere bruker.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+        binding.buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.sendPasswordResetEmail(binding.editUser.getText().toString())
                         .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(getContext(), "Email med oppretting av passord sendt.", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getContext(), "Email med oppretting av passord sendt.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
     }
