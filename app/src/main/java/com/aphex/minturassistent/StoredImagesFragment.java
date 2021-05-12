328623fc13a6946d4b8fa8dd4788e2dfa42a2dda
Are Abrahamsen <aab057@uit.no>
2021-05-12 14:01:55 +0200

    Laget adapter ++ til fragment_stored_images, med jørgen og halil.

--- app/src/main/java/com/aphex/minturassistent/StoredImagesFragment.java
+++ app/src/main/java/com/aphex/minturassistent/StoredImagesFragment.java
@@ -1,15 +1,32 @@
 package com.aphex.minturassistent;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.room.util.FileUtil;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class StoredImagesFragment extends Fragment {
-
+    // PICK_PHOTO_CODE is a constant integer
+    public final static int PICK_PHOTO_CODE = 1046;
 
     public StoredImagesFragment() {
     }
@@ -24,7 +41,57 @@ public class StoredImagesFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_stored_images, container, false);
 
-
         return view;
+    }
+
+    // Trigger gallery selection for a photo
+    public void onPickPhoto(View view) {
+        // Create intent for picking a photo from the gallery
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            // Bring up gallery to select a photo
+            startActivityForResult(intent, PICK_PHOTO_CODE);
+        }
+    }
+
+    public Bitmap loadFromUri(Uri photoUri) {
+        Bitmap image = null;
+        try {
+            // check version of Android on device
+            if(Build.VERSION.SDK_INT > 27){
+                // on newer versions of Android, use the new decodeBitmap method
+                ImageDecoder.Source source = ImageDecoder.createSource(getActivity().getContentResolver(), photoUri);
+                image = ImageDecoder.decodeBitmap(source);
+            } else {
+                // support older versions of Android by using getBitmap
+                image = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), photoUri);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if ((data != null) && requestCode == PICK_PHOTO_CODE) {
+            Uri photoUri = data.getData();
+
+            // Load the image located at photoUri into selectedImage
+            Bitmap selectedImage = loadFromUri(photoUri);
+
+            // Load the selected image into a preview
+            ImageView ivPreview = getActivity().findViewById(R.id.imageRecycler);
+            ivPreview.setImageBitmap(selectedImage);
+        }
     }
 }
---
+++ app/src/main/java/com/aphex/minturassistent/adapters/ImageAdapter.java
@@ -0,0 +1,119 @@
+package com.aphex.minturassistent.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStore;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.aphex.minturassistent.DetailsFragment;
+import com.aphex.minturassistent.Entities.Location;
+import com.aphex.minturassistent.Entities.Trip;
+import com.aphex.minturassistent.MainActivity;
+import com.aphex.minturassistent.R;
+import com.aphex.minturassistent.viewmodel.ViewModel;
+
+import org.jetbrains.annotations.NotNull;
+
+
+public class ImageAdapter extends ListAdapter<Image, ImageViewHolder> {
+    private LayoutInflater layoutInflater;
+    private ViewModel mViewModel;
+    Context context;
+
+
+    public ImageAdapter(Context context, @NonNull DiffUtil.ItemCallback<Image> diffCallback) {
+        super(diffCallback);
+        layoutInflater = (LayoutInflater)
+                context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = layoutInflater.inflate(R.layout.recyclerview_images_item, null);
+
+        ImageViewHolder holder = new ImageViewHolder(view, new ImageViewHolder.MyClickListener() {
+            @Override
+            public void onEdit(int position, View view) {
+//                Image click = getItem(position);
+//                int ImageID = click.getmImageID();
+//                SharedPreferences prefs = view.getContext().getSharedPreferences("tripID", 0);
+//                SharedPreferences.Editor editor = prefs.edit();
+//                prefs.edit().remove("tripID").apply();
+//                editor.putInt("tripID", tripID);
+//                editor.apply();
+
+//                Navigation.findNavController(view).navigate(R.id.detailsFragment);
+            }
+        });
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+        Image current = getItem(position);
+        String imageTitle = "Bildetekst: " ;
+
+        holder.tvImageTitle.setText(imageTitle);
+    }
+
+    public static class WordDiff extends DiffUtil.ItemCallback<Image> {
+        @Override
+        public boolean areItemsTheSame(@NonNull Image oldItem, @NonNull Image newItem) {
+            return oldItem == newItem;
+        }
+        @Override
+        public boolean areContentsTheSame(@NonNull Image oldItem, @NonNull Image newItem) {
+            return true;
+            //return oldItem.getmName().equals(newItem.getmName());
+        }
+    }
+}
+
+class ImageViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    MyClickListener listener;
+
+    public TextView tvImageTitle;
+    public CardView imageCardView;
+
+    ImageViewHolder(View itemView, MyClickListener listener) {
+        super(itemView);
+
+        tvImageTitle = itemView.findViewById(R.id.tvImageTitle);
+        imageCardView = itemView.findViewById(R.id.imageCardView);
+        this.listener = listener;
+
+        imageCardView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.imageCardView:
+                listener.onEdit(this.getLayoutPosition(), view);
+                break;
+            default:
+                break;
+        }
+    }
+    public interface MyClickListener {
+
+        void onEdit(int p, View view);
+    }
+}
