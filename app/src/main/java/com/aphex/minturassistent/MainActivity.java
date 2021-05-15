75ca33dcce93e2b235f9da6ac2fd50d1388ec7f2
Halil Ibrahim <hke006@uit.no>
2021-05-15 17:23:12 +0200

    Permission media ok

--- app/src/main/java/com/aphex/minturassistent/MainActivity.java
+++ app/src/main/java/com/aphex/minturassistent/MainActivity.java
@@ -3,23 +3,23 @@ package com.aphex.minturassistent;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.preference.PreferenceManager;
 
@@ -35,32 +35,39 @@ import org.osmdroid.config.Configuration;
 public class MainActivity extends AppCompatActivity {
     FusedLocationProviderClient mFusedLocationClient;
     Location mLastLocation;
-    private ActivityMainBinding binding;
-    private ViewModel mViewModel;
-    public final int REQUEST_LOCATION_PERMISSION = 1;
+    public final int LOCATION_PERMISSION_CODE = 0;
+    public final int ACCESS_MEDIA_LOCATION_CODE = 1;
     private static BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        getLocation();
+
+        //CHECK / GET PERMISSION TO USE FINELOCATION AND MEDIA LOCATION
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {
+                            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_MEDIA_LOCATION},
+                    LOCATION_PERMISSION_CODE);
+        } else {
+            Log.d("GET LOCATION", "getLocation: permissions granted");
+        }
+
         Configuration.getInstance().load(getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
 
         LayoutInflater layoutInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-        binding = ActivityMainBinding.inflate(layoutInflater);
+        com.aphex.minturassistent.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(layoutInflater);
         setContentView(binding.getRoot());
 
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.nav_host_fragment);
+        assert navHostFragment != null;
         NavController navController = navHostFragment.getNavController();
 
-        //BUTTOM NAVIGATION
         bottomNav = findViewById(R.id.bottom_nav);
         NavigationUI.setupWithNavController(bottomNav, navHostFragment.getNavController());
 
-        mViewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication())).get(ViewModel.class);
-
+        ViewModel mViewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication())).get(ViewModel.class);
     }
 
     public static void hideBottomNav() {
@@ -79,47 +86,74 @@ public class MainActivity extends AppCompatActivity {
 
     //Tatt fra: https://developer.android.com/codelabs/advanced-android-training-device-location?index=..%2F..advanced-android-training#3
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
-            case REQUEST_LOCATION_PERMISSION:
-                // If the permission is granted, get the location,
-                // otherwise, show a Toast
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            case LOCATION_PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     getLocation();
                 } else {
-                    Toast.makeText(this,
-                            R.string.location_permission_denied,
-                            Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, R.string.location_permission_denied, Toast.LENGTH_SHORT).show();
                 }
                 break;
+            }
+            case ACCESS_MEDIA_LOCATION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getMedia();
+                } else {
+                    Toast.makeText(this, R.string.media_permission_denied, Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
         }
     }
 
-    //Tatt fra: https://developer.android.com/codelabs/advanced-android-training-device-location?index=..%2F..advanced-android-training#3
     private void getLocation() {
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
+        //Tatt fra: https://developer.android.com/codelabs/advanced-android-training-device-location?index=..%2F..advanced-android-training#3
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED) {
             ActivityCompat.requestPermissions(this, new String[]
-                            {Manifest.permission.ACCESS_FINE_LOCATION},
-                    REQUEST_LOCATION_PERMISSION);
+                    {Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_CODE);
         } else {
-            mFusedLocationClient.getLastLocation().addOnSuccessListener(
-                    new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            Toast.makeText(MainActivity.this, "hey", Toast.LENGTH_SHORT).show();
-                            if (location != null) {
-                                mLastLocation = location;
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    Toast.makeText(MainActivity.this, R.string.location_permission_approved, Toast.LENGTH_SHORT).show();
+                    if (location != null) {
+                        mLastLocation = location;
+                    } else {
+                        //mLocationTextView.setText(R.string.no_location);
+                    }
+                }
+            });
+        }
+    }
 
-                            } else {
-                                //mLocationTextView.setText(R.string.no_location);
-                            }
-                        }
-                    });
+    private void getMedia() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_MEDIA_LOCATION) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                    {Manifest.permission.ACCESS_MEDIA_LOCATION}, ACCESS_MEDIA_LOCATION_CODE);
+        } else {
+            Toast.makeText(this, R.string.location_permission_approved, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean isMediaPermissionsGranted() {
+        //https://www.youtube.com/watch?v=_IbfUJS13h8
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.R) {
+            //For Android 11
+            return Environment.isExternalStorageManager();
+        } else {
+            //For Below
+            int externalStoragePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_MEDIA_LOCATION);
+            return externalStoragePermission == PackageManager.PERMISSION_GRANTED;
         }
     }
 }
+
+//MEDIA STORE
+//https://developer.android.com/training/data-storage/shared/media
+//if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+//        collection = MediaStore.Video.Media.getContentUri(MediaStore.VOLUME_EXTERNAL);
+//        } else {
+//        collection = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+//        }
